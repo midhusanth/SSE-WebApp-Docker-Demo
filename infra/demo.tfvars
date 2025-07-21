3386409e22f6cc93939f64555da9c1e00193132c
midhusanth <midhunsanthoshkumar@gmail.com>
2025-07-21 20:24:44 +0100

    removed rg2

--- infra/demo.tfvars
+++ infra/demo.tfvars
@@ -1,6 +1,3 @@
 resource_group_name = "rg-webapp-docker-demo"
 location            = "UK South"
 
-
-resource_group_name2 = "rg-webapp-docker-demo22222"
-
--- infra/resource_group.tf
+++ infra/resource_group.tf
@@ -6,13 +6,3 @@ module "resourcegroup" {
   location            = var.location
 
 }
-
-module "resourcegroup2" {
-  source  = "app.terraform.io/MG-WepApp-Docker-Demo/resourcegroup/azurerm"
-  version = "1.0.1"
-
-  resource_group_name = var.resource_group_name2
-  location            = var.location
-
-}
-
