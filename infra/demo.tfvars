fbff598f0be38ae49b5139b49f4f09190c52b3d1
midhusanth <midhunsanthoshkumar@gmail.com>
2025-07-21 19:34:10 +0100

    rg2 added

--- infra/demo.tfvars
+++ infra/demo.tfvars
@@ -1,2 +1,6 @@
 resource_group_name = "rg-webapp-docker-demo"
 location            = "UK South"
+
+
+resource_group_name2 = "rg-webapp-docker-demo2222"
+location            = "UK South"
--- infra/resource_group.tf
+++ infra/resource_group.tf
@@ -8,3 +8,12 @@ module "resourcegroup" {
 }
 
 
+module "resourcegroup2" {
+  source  = "app.terraform.io/MG-WepApp-Docker-Demo/resourcegroup/azurerm"
+  version = "1.0.1"
+
+  resource_group_name = var.resource_group_name2
+  location            = var.location
+
+}
+
