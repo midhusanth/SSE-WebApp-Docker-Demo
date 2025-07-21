a1142d343293d73033058bdf0c1d87a71c60e81e
midhusanth <midhunsanthoshkumar@gmail.com>
2025-07-21 18:10:19 +0100

    Formatted rg.tf

--- infra/resource_group.tf
+++ infra/resource_group.tf
@@ -1,10 +1,10 @@
 module "resourcegroup" {
   source  = "app.terraform.io/MG-WepApp-Docker-Demo/resourcegroup/azurerm"
   version = "1.0.1"
-  
+
   resource_group_name = var.resource_group_name
-  location = var.location
-  
+  location            = var.location
+
 }
 
 
