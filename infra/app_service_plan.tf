26f65ab9390f9cc51a02df4f240938f2e0a6cb56
midhusanth <midhunsanthoshkumar@gmail.com>
2025-07-27 12:02:23 +0100

    Infra: remvoed old resrouces

--- infra/app_service_plan.tf
+++ infra/app_service_plan.tf
@@ -1,13 +1,13 @@
-module "app_service_plan" {
-  source = "./modules/app_service_plan"
+# module "app_service_plan" {
+#   source = "./modules/app_service_plan"
 
-  name                = var.asp_name
-  location            = var.location
-  resource_group_name = module.resourcegroup.resource_group_name
-  app_kind            = var.asp_appkind
-  sku_tier            = var.asp_sku_tier
-  sku_size            = var.asp_sku_size
-}
+#   name                = var.asp_name
+#   location            = var.location
+#   resource_group_name = module.resourcegroup.resource_group_name
+#   app_kind            = var.asp_appkind
+#   sku_tier            = var.asp_sku_tier
+#   sku_size            = var.asp_sku_size
+# }
 
 
 module "asp_docker_demo" {
--- infra/azure_container_registry.tf
+++ infra/azure_container_registry.tf
@@ -1,12 +1,12 @@
-module "acr_webapp_demo" {
-  source = "./modules/azure_container_registry"
+# module "acr_webapp_demo" {
+#   source = "./modules/azure_container_registry"
 
-  acr_name            = var.acr_name
-  location            = var.location
-  resource_group_name = module.resourcegroup.resource_group_name
-  sku                 = var.acr_sku
-  admin_enabled       = var.acr_admin_enabled
-}
+#   acr_name            = var.acr_name
+#   location            = var.location
+#   resource_group_name = module.resourcegroup.resource_group_name
+#   sku                 = var.acr_sku
+#   admin_enabled       = var.acr_admin_enabled
+# }
 
 module "acr_docker_demo" {
   source = "./modules/azure_container_registry_v2"
