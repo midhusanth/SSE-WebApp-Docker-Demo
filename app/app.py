b516787387df0a797340ac55bd150f7a10cab255
midhusanth <midhunsanthoshkumar@gmail.com>
2025-07-27 16:38:11 +0100

    App: version 6.2

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return jsonify(status="ok... v6.1")
+    return jsonify(status="ok... v6.2")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
